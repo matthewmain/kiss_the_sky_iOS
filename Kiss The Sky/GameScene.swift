7466685c7019365375648534396cf9aace30aaee
Matthew Main <matthewwmain@gmail.com>
2019-05-18 19:33:43 -0400

    first commit (blank Xcode SpriteKit project)

---
+++ Kiss The Sky/GameScene.swift
@@ -0,0 +1,21 @@
+//
+//  GameScene.swift
+//  Kiss The Sky
+//
+//  Created by Matthew Main on 5/18/19.
+//  Copyright © 2019 Matthew Main. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    var entities = [GKEntity]()
+    var graphs = [String : GKGraph]()
+    
+    override func sceneDidLoad() {
+
+    }
+
+}
